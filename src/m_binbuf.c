80e7f2eacc908a2a5992ad0e64eebe8cecc262af
Mike Will <myQwil@gmail.com>
2022-11-10 07:17:05 -0500

    use custom atoi function strtoi
    very fast like atoi, but also returns an end pointer like strtol

--- src/m_binbuf.c
+++ src/m_binbuf.c
@@ -27,6 +27,26 @@ static inline int is_digit(char c)
     return ('0' <= c && c <= '9');
 }
 
+    /* fast atoi implementation with end pointer */
+static int strtoi(const char *s, char **end)
+{
+    int neg, n;
+    if (*s == '-') {
+        if (is_digit(s[1]))
+            neg = 1, n = (s[1] - '0'), s += 2;
+        else {
+            if (end) *end = (char *)s;
+            return 0;
+        }
+    }
+    else neg = 0, n = 0;
+
+    for (; is_digit(*s); ++s)
+        n = 10*n + (*s - '0');
+    if (end) *end = (char *)s;
+    return neg ? -n : n;
+}
+
 
 struct _binbuf
 {
@@ -182,7 +202,7 @@ void binbuf_text(t_binbuf *x, const char *text, size_t size)
                     if (!is_digit(*bufp))
                         dollar = 0;
                 if (dollar)
-                    SETDOLLAR(ap, atoi(buf+1));
+                    SETDOLLAR(ap, strtoi(buf+1, NULL));
                 else SETDOLLSYM(ap, gensym(buf));
             }
             else SETSYMBOL(ap, gensym(buf));
@@ -415,12 +435,7 @@ void binbuf_restore(t_binbuf *x, int argc, const t_atom *argv)
                     if (dollsym)
                         SETDOLLSYM(ap, usestr == str ?
                             argv->a_w.w_symbol : gensym(usestr));
-                    else
-                    {
-                        int dollar = 0;
-                        sscanf(usestr + 1, "%d", &dollar);
-                        SETDOLLAR(ap, dollar);
-                    }
+                    else SETDOLLAR(ap, strtoi(usestr+1, NULL));
                 }
                 else SETSYMBOL(ap, usestr == str ?
                     argv->a_w.w_symbol : gensym(usestr));
@@ -492,24 +507,16 @@ int canvas_getdollarzero(void);
 static int binbuf_expanddollsym(const char *s, char *buf, t_atom *dollar0,
     int ac, const t_atom *av, int tonew)
 {
-    int argno = (int)atol(s);
-    int arglen = 0;
-    const char *cs = s;
-    char c = *cs;
+    char *cs;
+    int argno = strtoi(s, &cs);
 
     *buf=0;
-    while (is_digit(c))
-    {
-        c = *cs++;
-        arglen++;
-    }
-
-    if (cs==s)      /* invalid $-expansion (like "$bla") */
+    if (argno < 0 || cs == s)      /* invalid $-expansion (like "$bla") */
     {
         sprintf(buf, "$");
         return 0;
     }
-    else if (argno < 0 || argno > ac) /* undefined argument */
+    else if (argno > ac) /* undefined argument */
     {
         if (!tonew)
             return 0;
@@ -525,7 +532,7 @@ static int binbuf_expanddollsym(const char *s, char *buf, t_atom *dollar0,
         }
         else atom_string(dollarvalue, buf, MAXPDSTRING/2-1);
     }
-    return (arglen-1);
+    return (cs - s);
 }
 
 /* expand any '$' variables in the symbol s.  "tonow" is set if this is in the
